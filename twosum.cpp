7af7185c54342a404b7ac32a0c86ccb4c7e982f7
johnny <johnny@localhost.localdomain>
2014-12-30 00:34:34 +0800

    unfinished

---
+++ twosum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		
+	}
+};
+
+int main(void) {
+	int v1[10] = {1,5,9,2,5,7,4,3};
+	Solution s;
+	vector<int> case1(&v1[0], &v1[7]); 
+
+	vector<int>::iterator it;
+	for(it = case1.begin(); it != case.end(); it++)
+		cout << *it << endl;
+	return 0;
+}
